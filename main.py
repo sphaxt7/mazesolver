de64f2a81d2f378c66c4accbf4515b30b97e7b04
sphaxt7 <issamtoxico@gmail.com>
2024-11-28 00:55:00 +0100

    perfect

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from window import *
+from graphics import *
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    # l = Line(Point(0, 0), Point(800, 600))
+    # win.draw_line(l, "black")
 
+    cell = Cell(win)
+    cell.draw(50, 50, 100, 100)
+    
+    win.wait_for_close()
 
 main()
