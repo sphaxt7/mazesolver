a0547d4cfc5b37132bcbb43d363bbd923aebf197
sphaxt7 <issamtoxico@gmail.com>
2024-11-28 04:30:03 +0100

    done

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from graphics import *
-from cell import Cell
+from graphics import Window
+from maze import Maze
+import sys
+import random
 
 def main():
-    win = Window(800, 600)
-    # l = Line(Point(0, 0), Point(800, 600))
-    # win.draw_line(l, "black")
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
 
-    cell = Cell(win)
-    cell.draw(50, 50, 100, 100)
-    
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    print("maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
     win.wait_for_close()
+
 
 main()
